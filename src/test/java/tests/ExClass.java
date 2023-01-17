7f5f37801c8f94ad2b34fd935932c656f85ca467
Ege Iper <ege.iper@launchmetrics.com>
2023-01-17 18:04:34 +0300

    commit

--- src/test/java/tests/ExClass.java
+++ src/test/java/tests/ExClass.java
@@ -1,13 +1,6 @@
 package tests;
 
-
-
-import org.egeiper.DriverUtils;
-import org.egeiper.ParameterStoreUtils;
 import org.egeiper.PropertyUtils;
-import org.egeiper.util.model.BrowserType;
-import org.egeiper.util.model.DriverType;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class ExClass {
