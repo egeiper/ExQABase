edfe952b490c0570c760fed651682226148b3684
Ege Iper <ege.iper@launchmetrics.com>
2023-01-17 17:11:37 +0300

    commit

--- src/test/java/tests/ExClass.java
+++ src/test/java/tests/ExClass.java
@@ -5,12 +5,16 @@ package tests;
 import org.egeiper.DriverUtils;
 import org.egeiper.util.model.BrowserType;
 import org.egeiper.util.model.DriverType;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class ExClass {
 
     @Test
     public void ege() {
-        DriverUtils.getDriver(DriverType.LOCAL, BrowserType.CHROME).get("https://google.com");
+       WebDriver driver = DriverUtils.getDriver(DriverType.LOCAL, BrowserType.CHROME);
+       driver.get("https://google.com");
+       driver.quit();
+
     }
 }
