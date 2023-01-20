95ef7986822c9dd45e4fd568a8f278c5926c9036
Ege Iper <ege.iper@launchmetrics.com>
2023-01-20 13:12:20 +0300

    commit

---
+++ shFiles/healthcheckWithExecution.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Environment Variables
+# HUB_HOST
+# BROWSER
+# MODULE
+
+echo "Checking if hub is ready - $HUB_HOST"
+
+while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
+do
+	sleep 1
+done
+
+java -cp "target/selenium-docker.jar:target/selenium-docker-tests.jar:target/libs/*" -DHUB_HOST=$HUB_HOST org.testng.TestNG $MODULE
